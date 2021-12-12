9dfe56585f6ebb08971f1576d76993583dd26a9d
Andrew <andrewnorilsk@gmail.com>
2021-12-13 00:23:19 +0700

    Create lower.go

---
+++ lower.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type Color string
+
+const (
+	ColorGreen = "\u001b[32m"
+	ColorReset = "\u001b[0m"
+)
+
+func colorize(color Color, message string) {
+	fmt.Println(string(color), message, string(ColorReset))
+}
+
+func main() {
+	var textBuffer []string
+	printVersion := flag.Bool("v", false, "Print version")
+	flag.Parse()
+
+	if *printVersion {
+		colorize(ColorGreen, "lower - text case converter, version 0.0.1 😉")
+		return
+	}
+	if len(os.Args) > 1 {
+		textBuffer = append(textBuffer, os.Args[1])
+	} else {
+		scan := bufio.NewScanner(os.Stdin)
+		for scan.Scan() {
+			textBuffer = append(textBuffer, scan.Text())
+		}
+		if err := scan.Err(); err != nil {
+			log.Println(err)
+		}
+	}
+
+	for _, sentence := range textBuffer {
+		fmt.Println(strings.ToLower(sentence))
+	}
+}
